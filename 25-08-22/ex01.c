ab57759389e7e6f3e2dbd8698bf9d3946e2d4bf2
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-10-27 09:59:20 -0300

    Update 4.1
    - Created ex01.c

---
+++ 25-08-22/ex01.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  //printf(), scanf()
+#include<stdlib.h> //system()
+#include<locale.h> //setlocale()
+
+main(){
+    setlocale(LC_ALL, "Portuguese");
+    
+    // Declaração de variáveis
+    // int-%d , float-%f, double-%lf, char-%c
+    int idade1, idade2, soma;
+
+    printf("Digite a idade 1: ");
+    scanf("%d", &idade1);
+    printf("Digite a idade 2: ");
+    scanf("%d", &idade2);
+
+    soma= idade1 + idade2;
+    printf("A soma é igual a %d.", soma);
+    printf("\n\n");
+    system("pause");
+}
